9a0f7457d01eabc730b77b87b786a22552300c2a
Ezzehi nour <ezzehinour2002@gmail.com>
2023-06-14 14:13:00 -0700

    first cprogram

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 	/**
 	 * main - THIS FUNCTION PRINTS "Programming is like building
-	 * a multilingual 
-	 * puzzle
+	 * 	a multilingual 
+	 *	puzzle
 	 *  
 	 */
 int main(void)
 {
 
 	puts("\"Programming is like building a multilingual puzzle");
-
 	return (0);
 }
