34c91190ce3d2cfab74cd7392fd5715041bdacb6
Ezzehi nour <ezzehinour2002@gmail.com>
2023-06-14 14:14:42 -0700

    first c program

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,9 +1,6 @@
 #include <stdio.h>
 	/**
-	 * main - THIS FUNCTION PRINTS "Programming is like building
-	 * 	a multilingual 
-	 *	puzzle
-	 *  
+	 *	main - print Programming is like building a multilingual puzzle
 	 */
 int main(void)
 {
