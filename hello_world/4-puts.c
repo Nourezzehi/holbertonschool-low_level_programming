0230994b9d19e36362cd96697480ea186621c391
Ezzehi nour <ezzehinour2002@gmail.com>
2023-06-14 23:45:58 -0700

    first

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -4,12 +4,13 @@
 
 
 #include <stdio.h>
-	/**
-	 *main - print Programming is like building a multilingual puzzle.
-	 *
-	 *Return: 0.
-	 */
 
+/**
+ * main - Prints "Programming is like building a multilingual
+ *                puzzle, followed by a new line.
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
 
