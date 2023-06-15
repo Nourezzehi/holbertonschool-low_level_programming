60c462d0e4c4c37593410cd0d03216fa0bb979d7
Ezzehi nour <ezzehinour2002@gmail.com>
2023-06-15 02:28:08 -0700

    hello

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -6,8 +6,10 @@
  * main - Prints "with proper grammar, but the outcome
  * is a piece of art," .
  *
- * Return value : 0 .
+ * Return: 0.
  */
+
+
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
