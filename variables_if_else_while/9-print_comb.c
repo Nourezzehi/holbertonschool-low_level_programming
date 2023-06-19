324b93765c2eb9220fdbe9f888b47d441c796b54
Ezzehi nour <ezzehinour2002@gmail.com>
2023-06-19 05:48:42 -0700

    comb

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - all numbers seperated by ','
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	i = 48;
+	while (i <= 57)
+	{
+		putchar(i);
+		if (i != 57)
+		{
+		putchar(',');
+		putchar(' ');
+		}
+	}
+	return (0);
+}
