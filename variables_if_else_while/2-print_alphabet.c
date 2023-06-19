1029a58cb5bbda351ca61bfe0e21ddd6f54fbed2
Ezzehi nour <ezzehinour2002@gmail.com>
2023-06-19 03:55:16 -0700

    just lowercase

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ *main - prints the alphabet
+ *in lowercase
+ *
+ * Return: 0;
+ */
+
+int main(void)
+{
+	char c;
+
+	c = 'a';
+	k = 'A';
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+	return (0);
+}
