df115510979f552dc7c6275633fd8cbe6db4abb5
Ezzehi nour <ezzehinour2002@gmail.com>
2023-06-19 05:32:08 -0700

    reverse aplpha

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - prints alphabet
+ * in reverse
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char c;
+
+	c = 'z';
+	while (c >= 'a')
+	{
+		putchar(i);
+		i--;
+	}
+	return (0);
+}
