c032fd4e74a894d39cf0505d655f6cc50083f57d
Ezzehi nour <ezzehinour2002@gmail.com>
2023-06-20 01:38:39 -0700

    _putchar first use

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - prints "_putchar"
+ *
+ * Return: 0
+ */
+
+
+int main(void)
+{
+for (i = 0; str[i] != '\0'; i++)
+	_putchar(str[i]);
+	return (0);
+}
---
+++ functions_nested_loops/main.h
@@ -0,0 +1,10 @@
+#ifndef MYHEADER_H
+#define MYHEADER_H
+
+/* Function declaration*/
+int _putchar (char c);
+/*variables declaratin*/
+int i;
+char str [25];
+
+#endif
