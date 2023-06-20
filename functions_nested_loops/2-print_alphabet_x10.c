917f177f0a89427f9a2d29f179af7e4745964567
Ezzehi nour <ezzehinour2002@gmail.com>
2023-06-20 02:43:10 -0700

    third

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include <unistd.h>
+#include "main.h"
+
+void print_alphabet_x10(void)
+{
+	char c = 'a';
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		while (c <= 'z')
+		{
+			_putchar(c);
+			c++;
+			_putchar('\n');
+		}
+	c = 'a';
+	}
+}	
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -4,4 +4,5 @@
 /* Function declaration*/
 int _putchar (char k);
 void print_alphabet(void);
+void print_alphabet_x10(void);
 #endif
