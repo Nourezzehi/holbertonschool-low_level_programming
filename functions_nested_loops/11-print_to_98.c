14c83276f1180a2f373f4e823306f36c26bf9846
Ezzehi nour <ezzehinour2002@gmail.com>
2023-06-21 02:32:43 -0700

    all numbers

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,63 @@
+#include "main.h"
+
+/**
+ * print_to_98 - print to a given number
+ *
+ *@n: the number to start counting from
+ */
+
+void print_to_98(int n)
+{
+	int i;
+
+	if (n <= 98)
+	{
+		for (i = n; i < 98; i++)
+		{
+		if (i < 0)
+		{
+			_putchar('-');
+			if (i > (-10))
+				_putchar(-i + 48);
+			else
+			{
+				_putchar(((-i) / 10) + 48);
+				_putchar(((-i) % 10) + 48);
+			}
+		}
+		else
+		{
+			if (i < 10 && i >= 0)
+                        _putchar(i + 48);
+			else
+			{
+			_putchar((i / 10) + 48);
+			_putchar((i % 10) + 48);
+			}
+			_putchar(',');
+			_putchar(' ');
+			}
+		}
+		}
+		else if (n > 98)
+		{
+		for (i = n; i > 98; i--)
+		{
+			if (i == 99)
+			{
+				_putchar(i / 10 + 48);
+				_putchar(i % 10 + 48);
+			}
+		else 
+		{
+		_putchar((i / 100) + 48);
+		_putchar(((i % 100) / 10) + 48);
+		_putchar((i % 10) + 48);
+		}
+		_putchar(',');
+		_putchar(' ');
+		}
+		_putchar((n / 10) + 48);
+		_putchar((n % 10) + 48);
+	}
+	}
