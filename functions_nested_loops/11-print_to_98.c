e2d9accf1c5e31f8599b04ef408f8cf3b9ca3a76
Ezzehi nour <ezzehinour2002@gmail.com>
2023-06-22 10:48:02 -0700

    bet

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -3,9 +3,12 @@
 /**
  * print_to_98 - print to a given number
  *
+ * print_num_dig3 - print 3 dig numbers
+ *
+ * print_num_dig2 - print 2 dig numbers
+ *
  *@n: the number to start counting from
  */
-
 void print_num_dig3(int n)
 {
 	if (n < 0)
@@ -36,46 +39,40 @@ void print_num_dig2(int n)
 	}
 }
 
-void num_smaller_than_98(int n)
+void print_to_98(int n)
 {
-	int i;
-
-	for (i = n; i < 98; i++)
+	if (n < 98)
 	{
-		if (i < 0)
-		{
-			_putchar('-');
-                        if (i > (-10))
-				_putchar(-i + 48);
-                        else if (i > (-100))
-				print_num_dig2(i);
-			else
-			{
-				print_num_dig3(i);
-			}
-				_putchar(',');
-				_putchar(' ');
+	for (int i = n; i < 98; i++)
+	{
+	if (i < 0)
+	{
+		_putchar('-');
+		if (i > -10)
+			_putchar(-i + '0');
+		else
+		print_num_dig2(i);
 		}
 		else
 		{
-			if (i < 10 && i >= 0)
-				_putchar(i + 48);
-			else
+			if (i < 10)
+			_putchar(i + '0');
+		else
 			print_num_dig2(i);
-			_putchar(',');
-			_putchar(' ');
 		}
+		_putchar(',');
+		_putchar(' ');
+	}
+	}
+		_putchar(' ');			}
 		}}
-
-void num_greater_than_98(int n)
-{
-	int i;
-
-	for (i = n; i > 98; i--)
-	{
+	else
+		{
+			for (i = n; i > 98; i--)
+			{
 		if (i == 99)
-                       	print_num_dig2(i);
-                else
+			print_num_dig2(i);
+		else
 		{
 			print_num_dig3(i);
 		}
@@ -83,13 +80,7 @@ void num_greater_than_98(int n)
 			_putchar(' ');
 	}
 }
-
-void print_to_98(int n)
-{	if (n < 98)
-		num_smaller_than_98(n);
-	else
-		num_greater_than_98(n);
 	_putchar(9 + 48);
 	_putchar(8 + 48);
 	_putchar('\n');
-}
+	}
