866964c4ef130ec4d5e4ce6344dfb6d5a8cccb58
Ezzehi nour <ezzehinour2002@gmail.com>
2023-06-23 01:33:50 -0700

    digit

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _isupper - checks if the
+ * chracter is upper
+ *
+ * @n: the given number
+ *
+ * Return: 1 or 0;
+ */
+
+int _isdigit(int c)
+{
+	if (c <= 9 && c >= 0)
+		return (1);
+	else
+		return (0);
+}
--- more_functions_nested_loops/_isdigit.c
+++ more_functions_nested_loops/_isdigit.c
@@ -8,9 +8,9 @@
  * Return: 1 or 0
  */
 
-int _isdigit(char c)
+int _isdigit(int c)
 {
-	if (c <= '9' && c >= '0')
+	if (c <= 9 && c >= 0)
 		return (1);
 	else
 		return (0);
