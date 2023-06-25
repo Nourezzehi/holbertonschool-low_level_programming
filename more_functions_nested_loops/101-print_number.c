e7436e0c71cf2d873574890b3ceabdf39cb3f66b
Ezzehi nour <ezzehinour2002@gmail.com>
2023-06-25 12:39:40 -0700

    lala

--- more_functions_nested_loops/101-print_number.c
+++ more_functions_nested_loops/101-print_number.c
@@ -1,44 +1,26 @@
 #include "main.h"
 
 /**
- * print_2 - prints 2 digits numbers
+ * print - print any number
  *
- * @n: given number
+ * @n: the given num
  */
 
-void print_2(int n)
+void print(int n)
 {
-	_putchar(n / 10 + '0');
-	_putchar(n % 10 + '0');
+	int i, k = 0, j = 1, f = n;
+
+	while (f /= 10)
+		k++;
+	for (i = 0; i < k; i++)
+		j *= 10;
+	for (i = 0; i < k + 1; i++)
+	{
+		_putchar((n / j) + '0');
+		n %= j;
+		j /= 10;
+	}
 }
-
-/**
- * print_3 - print 3 digits numbers
- *
- * @n: given number
- */
-
-void print_3(int n)
-{
-	_putchar(n / 100 + '0');
-	_putchar((n % 100) / 10 + '0');
-	_putchar(n % 10 + '0');
-}
-
-/**
- * print_4 - print 4 digits numbers
- *
- * @n: given number
- */
-
-void print_4(int n)
-{
-	_putchar(n / 1000 + '0');
-	_putchar((n % 1000) / 100 + '0');
-	_putchar((n % 100) / 10 + '0');
-	_putchar(n % 10 + '0');
-}
-
 /**
  * print_number - print a given
  * number
@@ -51,24 +33,9 @@ void print_number(int n)
 	if (n < 0)
 	{
 		_putchar('-');
-		if (n > -10)
-			_putchar(-n + '0');
-		else if (n > -100)
-			print_2(-n);
-		else if (n > -1000)
-			print_3(-n);
-		else
-			print_4(-n);
+		print(-n);
 	}
 	else
-	{
-		if (n < 10)
-			_putchar(n + '0');
-		else if (n < 100)
-			print_2(n);
-		else if (n < 1000)
-			print_3(n);
-		else
-			print_4(n);
-	}
+		print(n);
+	_putchar('\n');
 }
