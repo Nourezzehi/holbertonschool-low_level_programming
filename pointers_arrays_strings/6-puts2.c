64d6898abbd924e4c5189d3dfa2d37d443b04f29
Ezzehi nour <ezzehinour2002@gmail.com>
2023-06-26 11:12:03 -0700

    puts

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * puts2 - print every other character
+ *
+ * @str: the given string
+ */
+
+void puts2(char *str)
+{
+	int i = 0;
+
+	while (*(str + i) != '\0' && *(str + i + 1) != '\0')
+	{
+		_putchar(*(str + i));
+		i += 2;
+	}
+	_putchar('\n');
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -7,6 +7,7 @@ void swap_int(int *, int*);
 int _strlen(char *);
 void _puts(char *);
 void print_rev(char *);
-void rev_string(char *s);
+void rev_string(char *);
+void puts2(char *);
 
 #endif
