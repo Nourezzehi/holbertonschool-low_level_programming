d8bd049373ba44a14193dc542a5f4a64284a994d
Ezzehi nour <ezzehinour2002@gmail.com>
2023-06-26 10:58:04 -0700

    rev string

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -29,7 +29,7 @@ void rev_string(char *s)
 	int i, len = _strlen(s);
 	char c;
 
-	for (i = 0; i <= (len / 2); i++)
+	for (i = 0; i < (len / 2); i++)
 	{
 		c = *(s + i);
 		*(s + i) = *(s + len - i - 1);
