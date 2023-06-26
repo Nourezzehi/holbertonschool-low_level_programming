e8d6d0f3cab803ce3e75be3519ffdf5ddd322530
Ezzehi nour <ezzehinour2002@gmail.com>
2023-06-26 11:23:12 -0700

    praa

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -10,10 +10,12 @@ void puts2(char *str)
 {
 	int i = 0;
 
-	while (*(str + i) != '\0')
+	while (*(str + i) != '\0' && *(str + i + 1) != '\0')
 	{
 		_putchar(*(str + i));
 		i += 2;
 	}
+	if (*(str + i + 1) == '\0')
+		_putchar(*(str + i));
 	_putchar('\n');
 }
