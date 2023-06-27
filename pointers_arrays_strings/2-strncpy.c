498f9d3a2601ec863b4eb2d65bd76aa57a879254
Ezzehi nour <ezzehinour2002@gmail.com>
2023-06-27 07:28:34 -0700

    strncpy

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,45 @@
+
+/**
+ * _strlen - calculates length of
+ * a given string
+ *
+ * @ch: given string
+ *
+ * Return: len
+ */
+
+int _strlen(char *ch)
+{
+	int i = 0, len = 0;
+	while (*(ch + i) != '\0')
+	{
+		len++;
+		i++;
+	}
+	return (len);
+}
+/**
+ * _strncpy - copy n characters
+ *
+ * @dest: destination
+ *
+ * @src: copy source
+ *
+ * @n: number of characters
+ *
+ * Return: copied to
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0, len = _strlen(dest);
+
+	while ((*src + i) != '\0' && i < n)
+	{
+		*(dest + i) = *(src + i);
+		i++;	
+	}
+	if (i >= len)
+		*(dest + i) = '\0';
+	return (dest);
+}
