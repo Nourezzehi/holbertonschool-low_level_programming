912c9b2a67e3fa0de96492d3cae83c9d702a3fe9
Ezzehi nour <ezzehinour2002@gmail.com>
2023-06-27 02:47:33 -0700

    copy a string

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -6,7 +6,7 @@
  *
  * @src: the string to copy
  *
- * Return: dest 
+ * Return: dest
  */
 
 char *_strcpy(char *dest, char *src)
@@ -17,7 +17,7 @@ char *_strcpy(char *dest, char *src)
 	{
 		*(dest + i) = *(src + i);
 		i++;
-	}	
-	*(dest + i + 1) = '\0';
+	}
+	*(dest + i) = '\0';
 	return (dest);
 }
