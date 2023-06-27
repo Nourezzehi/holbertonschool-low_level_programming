fd5f9dd670b2f400e590c16ac0391895c9131d80
Ezzehi nour <ezzehinour2002@gmail.com>
2023-06-27 07:43:35 -0700

    strncpy

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -34,7 +34,7 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i = 0, len = _strlen(dest);
 
-	while ((*src + i) != '\0' && i < n)
+	while (*(src + i) != '\0' && i < n)
 	{
 		*(dest + i) = *(src + i);
 		i++;	
