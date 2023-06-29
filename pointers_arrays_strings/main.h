4d93b88eb4e63faebd6ef24153813100a39ce616
Ezzehi nour <ezzehinour2002@gmail.com>
2023-06-29 15:23:59 -0700

    strspn

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -24,5 +24,6 @@ char *leet(char *);
 char *_memset(char *, char, unsigned int);
 char *_memcpy(char *, char *, unsigned int);
 char *_strchr(char *, char);
+unsigned int _strspn(char *, char *);
 
 #endif
