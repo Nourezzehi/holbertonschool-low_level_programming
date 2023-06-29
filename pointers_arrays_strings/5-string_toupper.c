debf3f4a16b577da0bd3502f70c960782690f2d2
Ezzehi nour <ezzehinour2002@gmail.com>
2023-06-29 03:09:24 -0700

    low to upp

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -15,7 +15,7 @@ char *string_toupper(char *ch)
 	while (*(ch + i) != '\0')
 	{
 		if (*(ch + i) >= 'a' && *(ch + i) <= 'z')
-		       *(ch + i) -= 32;	
+			*(ch + i) -= 32;
 		i++;
 	}
 	return (ch);
