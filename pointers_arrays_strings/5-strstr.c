54f52ce149f253be231f4257df5d1aa83040e17b
Ezzehi nour <ezzehinour2002@gmail.com>
2023-06-30 09:55:27 -0700

    strstr

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,39 @@
+#include <stddef.h>
+/**
+ * _strstr - finds and loctaes the substring
+ * in the main string
+ *
+ * @haystack: the main string
+ *
+ * @needle: the substring
+ *
+ * Return: sub
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	unsigned int i = 0, j;
+	char *p = haystack, *s;
+
+	while (*(needle + i) != '\0')
+	{
+		while (*(p) != '\0')
+		{	
+			if (*(needle + i) == *(p))
+			{
+				j = i;
+				s = p;
+				while (*(s) != '\0' && *(s) == *(needle + j))
+				{
+					s++;
+					j++;
+				}
+				if ((*s) == '\0')
+					return (p);
+			}
+			p++;
+		}
+			i++;
+	}
+	return (NULL);
+}
