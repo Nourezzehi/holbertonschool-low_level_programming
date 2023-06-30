bbc2cb627aef19c8cc811faf989c66e40ddff6e0
Ezzehi nour <ezzehinour2002@gmail.com>
2023-06-30 13:23:01 -0700

    chessboard

--- pointers_arrays_strings/7-print_chessboard.c
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -16,13 +16,7 @@ void print_chessboard(char (*a)[8])
 		for (j = 0; j < 8; j++)
 		{
 			c = (*(*(a + i) + j));
-			if (i < 2 || i > 5)
-			{
-				if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
-					_putchar(c);
-				else
-					_putchar (' ');
-			}
+			_putchar (c);
 		}
 		_putchar('\n');
 	}
