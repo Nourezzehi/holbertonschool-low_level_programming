3d075c0d4b0c130638ce26049bba8e2ab8de6729
Ezzehi nour <ezzehinour2002@gmail.com>
2023-06-30 13:19:14 -0700

    chessboard

---
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_chessboard - print a chessboard
+ *
+ * @board: the chessboard
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	char c;
+	int i, j;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			c = (*(*(a + i) + j));
+			if (i < 2 || i > 5)
+			{
+				if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+					_putchar(c);
+				else
+					_putchar (' ');
+			}
+		}
+		_putchar('\n');
+	}
+}
