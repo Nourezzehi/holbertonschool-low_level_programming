f39424478010dc1bfc6b14ad4b5910e82f010b2d
Ezzehi nour <ezzehinour2002@gmail.com>
2023-07-14 04:55:56 -0700

    strcat

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -19,6 +19,8 @@ char *str_concat(char *s1, char *s2)
 	ch = (char *) malloc(sizeof(char) * (strlen(s1) + strlen(s2) + 1));
 	if (ch != NULL)
 	{
+		if (s1 == NULL)
+			strcpy(s1, "");
 		strcpy(ch, s1);
 		strcat(ch, s2);
 	}
