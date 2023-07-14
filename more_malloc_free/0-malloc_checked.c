97553da776745b51ce7febcabe78c35c5cb0457f
Ezzehi nour <ezzehinour2002@gmail.com>
2023-07-14 10:20:55 -0700

    readme file

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -1,9 +1,11 @@
 #include <stdlib.h>
 
 /**
- * malloc_cheched - checks if memory is allocated
+ * malloc_checked - checks if memory is allocated
  *
  * @b: pointer to allocated memory
+ *
+ * Return: pointer
  */
 
 void *malloc_checked(unsigned int b)
@@ -11,7 +13,7 @@ void *malloc_checked(unsigned int b)
 	void *p;
 
 	p = malloc(b);
-	if( p == NULL)
+	if (p == NULL)
 		exit(98);
 	return (p);
 }
