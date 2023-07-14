5d7770982ee6f0bd13cd65e0c91a6c8a2f5a7873
Ezzehi nour <ezzehinour2002@gmail.com>
2023-07-14 07:57:12 -0700

    allocmatrix

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -17,8 +17,19 @@ int **alloc_grid(int width, int height)
 	if (height <= 0 || width <= 0)
 		return (NULL);
 	m = (int **) malloc(sizeof(int *) * height);
+	if (m == NULL)
+		return (NULL);
 	for (i = 0; i < width; i++)
+	{
 		*(m + i) = (int *) malloc(sizeof(int) * width);
+		if (*(m + i) == NULL)
+		{
+			for (; i >= 0; i--)
+				free(*(m + i));
+			free(m);
+			return (NULL);
+		}
+	}
 	for (i = 0; i < height; i++)
 	{
 		for (j = 0; j < width; j++)
