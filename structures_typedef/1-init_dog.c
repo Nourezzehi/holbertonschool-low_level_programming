ba450b74a89e8211103cd5763dba3a3bcd462b7b
Ezzehi nour <ezzehinour2002@gmail.com>
2023-07-17 02:32:03 -0700

    init a struct

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,24 @@
+#include <string.h>
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * init_dog - initialize the struct
+ *
+ * @d: dogs structure
+ *
+ * @name: dogs name
+ *
+ * @age: dogs age
+ *
+ * @owner: dogs owner
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d -> name = (char *) malloc(strlen(name) * sizeof(char) + 1);
+	d -> owner = (char *) malloc(strlen(owner) * sizeof(char) + 1);
+	strcpy(d -> name, name);
+	d -> age = age;
+	strcpy(d -> owner, owner);
+}
