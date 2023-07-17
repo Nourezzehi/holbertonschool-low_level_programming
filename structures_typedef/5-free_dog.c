a8c1db70f1b8e4af4ae6683a4d7906f42eba699e
Nourezzehi <nour.ezzhi.insat.ucar.tn>
2023-07-17 21:40:36 +0100

    free the dog

--- structures_typedef/5-free_dog.c
+++ structures_typedef/5-free_dog.c
@@ -9,13 +9,10 @@
 
 void free_dog(dog_t *dog)
 {
-	if (!dog)
-	{
-		free(dog);
-		return;
-	}
 	if (!dog->name)
 		free(dog->name);
 	if (!dog->owner)
 		free(dog->owner);
+	if (!dog)
+		free(dog);
 }
