b8da6e1f9141e7c7b18c2df402f2774a61f1f2ee
Nourezzehi <nour.ezzhi.insat.ucar.tn>
2023-07-17 20:57:27 +0100

    a new dog

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -7,24 +7,25 @@
  * @name: dogs name
  *
  * @age: dogs age
- * 
+ *
  * @owner: dogs owner
- * 
+ *
  * Return: dogs profile
 */
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-    dog_t *new_dog;
-    new_dog = malloc(sizeof(dog_t));
-    new_dog->name = (char *) malloc((strlen(name) + 1) * sizeof(char));
-    if (!new_dog->name)
-        return(NULL);
-    new_dog->owner = (char *) malloc((strlen(owner) + 1) * sizeof(char));
-    if (!new_dog->owner)
-        return(NULL);
-    new_dog->name = name;
-    new_dog->owner = owner;
-    new_dog->age = age;
-    return (new_dog);
+	dog_t *new_dog;
+
+	new_dog = malloc(sizeof(dog_t));
+	new_dog->name = (char *) malloc((strlen(name) + 1) * sizeof(char));
+	if (!new_dog->name)
+		return (NULL);
+	new_dog->owner = (char *) malloc((strlen(owner) + 1) * sizeof(char));
+	if (!new_dog->owner)
+		return (NULL);
+	new_dog->name = name;
+	new_dog->owner = owner;
+	new_dog->age = age;
+	return (new_dog);
 }
