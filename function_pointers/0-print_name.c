e73fc6ab2194b405579c2ddd6ad2d94a73064d99
Nourezzehi <nour.ezzhi.insat.ucar.tn>
2023-07-18 08:42:46 +0100

    print name

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,4 +1,5 @@
 #include "function_pointers.h"
+#include <stdlib.h>
 
 /**
  * print_name - print a name
@@ -10,6 +11,6 @@
 
 void print_name(char *name, void(*f)(char *))
 {
-	/*if (!name)*/	
+	if (f != NULL)	
 	f(name);
 }
