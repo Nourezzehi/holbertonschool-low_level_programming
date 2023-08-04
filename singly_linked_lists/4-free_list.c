7b72c19b3b406677cfeea620c95ac81ff215e03b
Nourezzehi <nour.ezzhi.insat.ucar.tn>
2023-08-04 14:02:00 +0100

    free

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_list - free the entire list
+ *
+ * @head: head of the list
+*/
+
+void free_list(list_t *head)
+{
+	list_t *before_list;
+
+	if (!head)
+		return;
+	while (head != NULL)
+	{
+		before_list = head;
+		head = head->next;
+		free(before_list);
+	}
+}
