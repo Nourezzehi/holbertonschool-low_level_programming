0945e93870660b360af07094bab4af09eb8d669d
Nourezzehi <nour.ezzhi.insat.ucar.tn>
2023-08-04 19:26:53 +0100

    insert pos

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -1,41 +1,52 @@
 #include "lists.h"
-
 /**
  * insert_dnodeint_at_index - Inserts a new node in a
  * list at a given position
- * @h: head of the list
- *
+ * @h: h of the list
  * @idx: The position to insert the new node
- *
  * @n: The integer to insert
- *
  * Return: the new list
  */
-
 list insert_dnodeint_at_index(list *h, unsigned int idx, int n)
 {
-	list list_move = *h, new;
+	unsigned int count;
+	list tmp, new, tmp_prev;
 
-	if (idx == 0)
-		return (add_dnodeint(h, n));
-
-	for (; idx != 1; idx--)
-	{
-		list_move = list_move->next;
-		if (list_move == NULL)
-			return (NULL);
-	}
-
-	if (list_move->next == NULL)
-		return (add_dnodeint_end(h, n));
-
+	if (h == NULL && idx > 0)
+	return (NULL);
 	new = (list) malloc(sizeof(dlistint_t));
 	if (new == NULL)
 		return (NULL);
-	new->n = n;
-	new->prev = list_move;
-	(list_move->next)->prev = new;
-	new->next = list_move->next;
-	list_move->next = new;
-	return (new);
+	new->n = n, new->prev = new->next = NULL;
+	if (idx == 0)
+	{
+		if (*h)
+	{
+		new->next = *h;
+		(*h)->prev = new, *h = new;
+	}
+		else
+			*h = new;
+		return (new);
+	}
+	count = 1, tmp = (*h)->next;
+	while (tmp)
+	{
+		if (idx == count)
+		{
+			tmp->prev->next = new, new->prev = tmp->prev;
+			new->next = tmp, tmp->prev = new;
+			return (new);
+		}
+		count++;
+		tmp_prev = tmp;
+		tmp = tmp->next;
+	}
+	if (tmp == NULL && count == idx)
+	{
+		tmp_prev->next = new, new->prev = tmp_prev;
+		return (new);
+	}
+	free(new);
+	return (NULL);
 }
