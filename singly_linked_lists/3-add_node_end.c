7bc5ffc148c87a3c351564ec01d2a46ef9e88031
Nourezzehi <nour.ezzhi.insat.ucar.tn>
2023-08-04 13:41:20 +0100

    end node

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -1,14 +1,14 @@
 #include "lists.h"
 
 /**
- * add_node_end - add a node at the
- * beginnig of the list
- *
- * @head: head pf the list
- *
- * @str: the string in the added node
- *
- * Return: new head
+* add_node_end - add a node at the
+* beginnig of the list
+*
+* @_t***head: **head pf the list
+*
+* @_ts*tr: the string in the added node
+*
+* Return: new **head
 */
 
 list_t *add_node_end(list_t **head, const char *str)
@@ -16,21 +16,21 @@ list_t *add_node_end(list_t **head, const char *str)
 	list_t *add, *aux = *head;
 
 	if (!str)
-		return (*head);
+		return (NULL);
 	add = (list_t *) malloc(sizeof(list_t));
 	if (!add)
-		return (*head);
+		return (NULL);
 	add->str = strdup(str);
 	if (!(add->str))
 	{
 		free(add);
-		return (*head);
+		return (NULL);
 	}
 	add->len = strlen(str);
 	add->next = NULL;
 	if (!(*head))
 	{
-		*(head) = add;
+		*head = add;
 		return (*head);
 	}
 	while (!(aux->next))
