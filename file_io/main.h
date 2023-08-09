0dde8aaeeea5c916c00baf724b4b5531df7c0576
Nourezzehi <nour.ezzhi.insat.ucar.tn>
2023-08-09 07:15:54 +0100

    read and write to a file

---
+++ file_io/main.h
@@ -0,0 +1,10 @@
+#ifndef HEADER_H
+#define HEADER_H
+
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+
+int _putchar(char);
+ssize_t read_textfile(const char *filename, size_t letters);
+#endif
