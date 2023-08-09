2be3f8fa12873f2f2c9a074309038de63c6eb053
Nourezzehi <nour.ezzhi.insat.ucar.tn>
2023-08-09 08:51:43 +0100

    create a file

---
+++ file_io/1-create_file.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * create_file - create a file
+ * @filename: name of the file to create
+ * @text_content: content to add to the newly created file
+ * Return: num of read letters added to file or -1 if failure
+*/
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd, w;
+
+	if (filename == NULL)
+		return (-1);
+	if (text_content == NULL)
+	{
+		fd = open(filename, O_RDWR | O_CREAT | O_TRUNC);
+		if (fd == -1)
+			return (fd);
+	}
+	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC);
+	if (fd == -1)
+		return (fd);
+	w = write(fd, text_content, strlen(text_content));
+	if (w == -1)
+		return (w);
+	return (1);
+}
