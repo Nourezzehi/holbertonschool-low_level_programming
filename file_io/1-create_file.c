959f946de1afb426c0ba359fa4293547fba26994
Nourezzehi <nour.ezzhi.insat.ucar.tn>
2023-08-09 10:21:58 +0100

    trial

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -20,7 +20,7 @@ int create_file(const char *filename, char *text_content)
 			return (fd);
 		return (1);
 	}
-	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 600);
+	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 00600);
 	if (fd == -1)
 		return (fd);
 	w = write(fd, text_content, strlen(text_content));
